57ed152d45328611c0366d0b88d6a33114f3b863
Denis Derkach <podstavka128@gmail.com>
2019-01-20 15:54:42 +0200

    Create Stack realization

---
+++ 10_Stack/index.js
@@ -0,0 +1,125 @@
+class Stack {
+    constructor() {
+        this.elements = [];
+        this.count = this.elements.length;
+    }
+
+    setCount() {
+        this.count = this.elements.length;
+    }
+
+    push(item) {
+        this.elements[this.elements.length] = item;
+        this.setCount();
+    }
+
+    pop() {
+        if (this.elements.length === 0) {
+            throw new Error('Stack is empty!');
+        }
+
+        let element = this.elements[this.elements.length - 1];
+        this.elements.length--;
+        this.setCount();
+        return element;
+    }
+
+    peek() {
+        if (this.elements.length === 0) {
+            throw new Error('Stack is empty!');
+        }
+
+        return this.elements[this.elements.length - 1];
+    }
+
+    clear() {
+        this.elements.length = 0;
+        this.setCount();
+    }
+
+    clone() {
+        return this;
+    }
+
+    contains(item) {
+        for (let i = 0; i < this.elements.length; i++) {
+            if (item === this.elements[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    toArray() {
+        return this.elements;
+    }
+
+    copyTo(array, index) {
+        if (!(array instanceof Array)) {
+            throw new Error(`${array} is not an Array!`);
+        }
+
+        if (index < 0) {
+            throw new Error('Index must be greater than 0!');
+        }
+
+        for (let i = 0; i < this.elements.length; i++) {
+            array[index] = this.elements[i];
+            index++;
+        }
+    }
+}
+
+//Test CONSTRUCTOR
+let s = new Stack();
+console.log(s);
+
+//Test PUSH
+s.push(5);
+s.push(10);
+s.push(15);
+console.log(s);
+
+//Test POP
+let s1 = s.pop();
+console.log(s);
+console.log(s1);
+
+//Test PEEK
+let s2 = s.peek();
+console.log(s);
+console.log(s2);
+
+//Test CLEAR
+s.clear();
+console.log(s);
+
+//Test PUSH again
+s.push(20);
+s.push(25);
+s.push(30);
+console.log(s);
+
+//Test CLONE
+let s3 = s.clone();
+console.log(s);
+console.log(s3);
+console.log(s === s3);
+
+//Test CONTAINS
+console.log(s.contains(3));
+console.log(s.contains(20));
+
+//Test TO_ARRAY
+let s4 = s.toArray();
+console.log(s);
+console.log(s4);
+console.log(s === s4.elements);
+
+//Test COPY_TO
+let arr = ['Hello', 'World'];
+let arr1 = ['Hello', 'World'];
+s.copyTo(arr, 1);
+console.log(arr);
+s.copyTo(arr1, 3);
+console.log(arr1);
